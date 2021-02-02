ce6fb7d81ca21bbae1991b4e350f313b886896e4
萩原志音 <shion@tateito.co.jp>
2021-02-02 20:51:10 +0900

    Initial Commit

---
+++ justpaint/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  justpaint
+//
+//  Created by 秋泉 on 2021/02/02.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
